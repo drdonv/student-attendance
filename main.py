fcf9169f383ac9e7a53e68f6a8c26fc12b03c0a7
drdonv <realdrdonv@gmail.com>
2022-09-05 20:03:33 -0700

    List practice

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+myList.append("Hello")
+
+myList.pop(3)
+
+myList.remove(9)
+
+
+print(myList)
+
+print(len(myList))
+
+print(myList[3:])
